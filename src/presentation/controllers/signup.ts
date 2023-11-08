2bb95d2956f644430b5ba9f47d30513d0b039cac
Erick Leandro <j.erickleandro@gmail.com>
2023-11-08 12:12:41 +0000

    refactor: insert required fields into handle function

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,11 +3,11 @@ import { MissingParamError } from "../errors";
 import { badRequest } from "../helpers/http-helper";
 export class SignupController {
   handle(httpRequest: httpRequest): httpResponse {
-    if(!httpRequest.body.name){
-      return badRequest(new MissingParamError("name"));
-    }
-    if(!httpRequest.body.email){
-      return  badRequest(new MissingParamError("email"));
-    }
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
   }
 }
